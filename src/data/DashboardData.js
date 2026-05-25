a5d7afb03c3782f8f70fcf088e696d4e57ded380
Abhinav <abhinav731996@gmail.com>
2026-05-25 10:37:58 +0530

    scss setup

---
+++ src/data/DashboardData.js
@@ -0,0 +1,235 @@
+// export const dashboardStats = [
+//   {
+//     id: 1,
+//     name: "Charger Cable - Lighting",
+//     price: "$187",
+//     sales: "247+ Sales",
+//     image: "/images/products/product-1.png",
+//     growth: "25%",
+//     status: "increase",
+//     stock: "0",
+//     category: "electronics",
+//     order: "",
+//   },
+// ];
+// // try to make a single data but that doesn't work
+
+
+// For items 
+export const topSellingProducts = [
+  {
+    id: 1,
+    name: "Charger Cable - Lighting",
+    price: "$187",
+    sales: "247+ Sales",
+    image: "/images/products/product-1.png",
+    growth: "25%",
+    status: "increase",
+  },
+  {
+    id: 2,
+    name: "Yves Saint Eau De Parfum",
+    price: "$145",
+    sales: "289+ Sales",
+    image: "/images/products/product-2.png",
+    growth: "25%",
+    status: "increase",
+  },
+  {
+    id: 3,
+    name: "Apple Airpods 2",
+    price: "$458",
+    sales: "300+ Sales",
+    image: "/images/products/product-3.png",
+    growth: "25%",
+    status: "increase",
+  },
+  {
+    id: 4,
+    name: "Vacuum Cleaner",
+    price: "$139",
+    sales: "225+ Sales",
+    image: "/images/products/product-4.png",
+    growth: "21%",
+    status: "decrease",
+  },
+  {
+    id: 5,
+    name: "Samsung Galaxy S21 Fe 5g",
+    price: "$898",
+    sales: "365+ Sales",
+    image: "/images/products/product-5.png",
+    growth: "25%",
+    status: "increase",
+  },
+];
+
+export const lowStockProducts = [
+  {
+    id: 1,
+    name: "Vacuum Cleaner Robot",
+    productId: "#940004",
+    stock: 21,
+    image: "/images/products/stock-1.png",
+  },
+  {
+    id: 2,
+    name: "Dell XPS 13",
+    productId: "#665814",
+    stock: 8,
+    image: "/images/products/stock-2.png",
+  },
+  {
+    id: 3,
+    name: "KitchenAid Stand Mixer",
+    productId: "#325569",
+    stock: 14,
+    image: "/images/products/stock-3.png",
+  },
+  {
+    id: 4,
+    name: "Levi's Trucker Jacket",
+    productId: "#124588",
+    stock: 12,
+    image: "/images/products/stock-4.png",
+  },
+  {
+    id: 5,
+    name: "Lay's Classic",
+    productId: "#365586",
+    stock: 10,
+    image: "/images/products/stock-5.png",
+  },
+];
+
+export const recentSales = [
+  {
+    id: 1,
+    product: "Apple Watch Series 9",
+    category: "Electronics",
+    amount: "$640",
+    date: "Today",
+    status: "Processing",
+    image: "/images/sales/sale-1.png",
+  },
+  {
+    id: 2,
+    product: "Gold Bracelet",
+    category: "Fashion",
+    amount: "$126",
+    date: "Today",
+    status: "Cancelled",
+    image: "/images/sales/sale-2.png",
+  },
+  {
+    id: 3,
+    product: "Parachute Down Duvet",
+    category: "Health",
+    amount: "$89",
+    date: "15 Jan 2025",
+    status: "Onhold",
+    image: "/images/sales/sale-3.png",
+  },
+  {
+    id: 4,
+    product: "YETI Rambler Tumbler",
+    category: "Sports",
+    amount: "$65",
+    date: "12 Jan 2025",
+    status: "Processing",
+    image: "/images/sales/sale-4.png",
+  },
+  {
+    id: 5,
+    product: "Osmo Genius Starter Kit",
+    category: "Lifestyles",
+    amount: "$87.56",
+    date: "11 Jan 2025",
+    status: "Completed",
+    image: "/images/sales/sale-5.png",
+  },
+];
+
+
+// for customers 
+export const recentTransactions = [
+  {
+    id: 1,
+    customer: "Andrea Willer",
+    orderId: "#114589",
+    date: "24 May 2025",
+    total: "$4560",
+    status: "Completed",
+    image: "/images/users/user-1.png",
+  },
+  {
+    id: 2,
+    customer: "Timothy Sands",
+    orderId: "#114589",
+    date: "23 May 2025",
+    total: "$3569",
+    status: "Completed",
+    image: "/images/users/user-2.png",
+  },
+  {
+    id: 3,
+    customer: "Benito Rodrigues",
+    orderId: "#114589",
+    date: "22 May 2025",
+    total: "$2659",
+    status: "Draft",
+    image: "/images/users/user-3.png",
+  },
+  {
+    id: 4,
+    customer: "Randy McCree",
+    orderId: "#114589",
+    date: "21 May 2025",
+    total: "$2155",
+    status: "Completed",
+    image: "/images/users/user-4.png",
+  },
+];
+
+export const topCustomers = [
+  {
+    id: 1,
+    name: "Carlos Curran",
+    country: "USA",
+    orders: 24,
+    amount: "$8965",
+    image: "/images/customers/customer-1.png",
+  },
+  {
+    id: 2,
+    name: "Stan Gaunter",
+    country: "UAE",
+    orders: 22,
+    amount: "$6985",
+    image: "/images/customers/customer-2.png",
+  },
+  {
+    id: 3,
+    name: "Richard Wilson",
+    country: "Germany",
+    orders: 14,
+    amount: "$5366",
+    image: "/images/customers/customer-3.png",
+  },
+  {
+    id: 4,
+    name: "Mary Bronson",
+    country: "Belgium",
+    orders: 8,
+    amount: "$4569",
+    image: "/images/customers/customer-4.png",
+  },
+  {
+    id: 5,
+    name: "Annie Tremblay",
+    country: "Greenland",
+    orders: 14,
+    amount: "$35,698",
+    image: "/images/customers/customer-5.png",
+  },
+];
